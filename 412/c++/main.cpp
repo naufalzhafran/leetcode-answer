b2044b555a41d1d4130b0fc813b28073112450e3
naufal.latif <naufal.latif@seamoney.com>
2023-06-14 09:43:38 +0700

    feat: add go and cpp for 412

---
+++ 412/c++/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+template <typename S>
+ostream &operator<<(ostream &os,
+                    const vector<S> &vector)
+{
+    // Printing all the elements
+    // using <<
+    for (auto element : vector)
+    {
+        os << element << " ";
+    }
+    return os;
+}
+
+vector<string> fizzBuzz(int n)
+{
+    vector<string> result;
+
+    map<int, string> RESULT_STRING{
+        {3, "Fizz"},
+        {5, "Buzz"},
+    };
+
+    for (int i = 1; i <= n; i++)
+    {
+        string tempItem = "";
+
+        for (auto element : RESULT_STRING)
+        {
+            if (i % element.first == 0)
+            {
+                tempItem += element.second;
+            }
+        }
+
+        if (tempItem == "")
+        {
+            tempItem = to_string(i);
+        }
+
+        result.push_back(tempItem);
+    }
+
+    return result;
+}
+
+int main()
+{
+    cout << fizzBuzz(15);
+}
