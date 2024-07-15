d683fbbf2d5b3e3657559509747bccd9242da0fd
naufal.latif <naufal.latif@seamoney.com>
2024-07-15 10:31:15 +0700

    feat: add 242 c++ answer

---
+++ 242. Valid Anagram/main.cpp
@@ -0,0 +1,32 @@
+#include <unordered_map>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool isAnagram(string s, string t)
+    {
+        unordered_map<char, int> count;
+
+        for (auto x : s)
+        {
+            count[x]++;
+        }
+
+        for (auto x : t)
+        {
+            count[x]--;
+        }
+
+        for (auto x : count)
+        {
+            if (x.second != 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
